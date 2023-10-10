e028784eb6f94bb36f6f606a0c31da99a0d540f9
patricmat <patmatome3037@gmail.com>
2023-10-10 15:29:11 -0700

    errors.c

---
+++ errors.c
@@ -0,0 +1,85 @@
+#include "shell.h"
+
+/**
+ * _eputs - an input string executed
+ * @str: the string to be executed
+ *
+ * Return: Nothing
+ */
+void _eputs(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_eputchar(str[i]);
+		i++;
+	}
+}
+
+/**
+ * _eputchar - inputs the character c to stderr
+ * @c: char to print
+ *
+ * Return: 1 success ,
+ * If fails, -1 is returned, and errno is set
+ */
+int _eputchar(char c)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(2, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ * _putfd - inputs the character c to given filedescriptor
+ * @c: The char to print
+ * @fd: The filedescriptor printed
+ *
+ * Return: 1 success ,
+ * If fails, -1 is returned, and errno is set appropriately.
+ */
+int _putfd(char c, int fd)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(fd, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ * _putsfd - executes an input string
+ * @str: the string to be executed
+ * @fd: the filedescriptor printed
+ *
+ * Return: number of characters inputed
+ */
+int _putsfd(char *str, int fd)
+{
+	int i = 0;
+
+	if (!str)
+		return (0);
+	while (*str)
+	{
+		i += _putfd(*str++, fd);
+	}
+	return (i);
+}
